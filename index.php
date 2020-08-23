948cda9e530b590db79366f950f93a99c719cd02
Shubham Chaudhary <shubhamchaudhary1399@gmail.com>
2020-08-23 17:46:54 +0530

    adding css

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Home | FoodShala</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:500&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/master.css">
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
 </head>
 <body>
-	<h2>Hello</h2>
+	<?php include 'includes/navbar.php'; ?>
 
+	<script type="text/javascript" src="js/navbar.js">
+
+	</script>
 </body>
 </html>
