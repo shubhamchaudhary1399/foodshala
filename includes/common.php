246f0bd91d968910596f3acd59dd79c8472970b2
Shubham Chaudhary <shubhamchaudhary1399@gmail.com>
2020-08-27 14:12:42 +0530

    final

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("us-cdbr-east-02.cleardb.com/", "bda4d464a3c93f", "bd18e408", "heroku_fcc36eb48604f4a")
+    $con = mysqli_connect("us-cdbr-east-02.cleardb.com", "bda4d464a3c93f", "bd18e408", "heroku_fcc36eb48604f4a")
     or die(mysqli_error($con));
     if(!isset($_SESSION)){
       session_start();
